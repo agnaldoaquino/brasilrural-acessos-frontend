080506d9debe856be12af955328e7bdc29f65c1c
Agnaldo Aquino <agnaldo.aquino@gmail.com>
2025-06-17 18:41:48 -0300

    Correção de Bag no componente URLCell.jsx

--- src/components/UrlCell.jsx
+++ src/components/UrlCell.jsx
@@ -1,29 +1,20 @@
 // components/UrlCell.jsx
-import { FiCopy } from "react-icons/fi";
-import { toast } from "react-toastify";
 
 const UrlCell = ({ url }) => {
   if (!url) return "-";
 
- return (
-  <div className="flex items-center w-full">
-    <a
-      href={url}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="flex-1 text-blue-600 hover:underline truncate"
-    >
-      {url}
-    </a>
-    <button
-      onClick={copiarParaAreaDeTransferencia}
-      title="Copiar URL"
-      className="p-1 rounded text-gray-700 hover:text-gray-900 hover:bg-gray-200 cursor-pointer ml-2"
-    >
-      <FiCopy className="w-4 h-4" />
-    </button>
-  </div>
-);
+  return (
+    <div className="flex items-center w-full">
+      <a
+        href={url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex-1 text-blue-600 hover:underline truncate"
+      >
+        {url}
+      </a>
+    </div>
+  );
 };
 
 export default UrlCell;
